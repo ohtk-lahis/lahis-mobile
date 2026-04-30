1e8f4c1aa64546a4ccc2c5ff59cbb456545afddd
pphetra <pphetra@gmail.com>
2026-04-30 13:18:24 +0700

    Upgrade Firebase mobile dependencies

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   file_selector_windows
   firebase_core
+  firebase_remote_config
   flutter_secure_storage_windows
   flutter_timezone
   geolocator_windows
